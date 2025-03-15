62de9b745830a16429031d78623bd362b6f4cbe3
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-03-15 10:56:08 +0900

    arithmetic & math

---
+++ arithmetic & math.py
@@ -0,0 +1,47 @@
+import math
+
+# 연산자: +, -, *, /, **, %
+friends = 0
+
+friends = friends + 4   # 4
+friends += 1            # 5
+friends -= 2            # 3
+friends *= 3            # 9
+friends /= 3            # 3
+friends **= 2           # 9
+friends %= 2            # 1
+
+print(friends)
+
+# built-in math related functions:
+x = 3.5
+y = -4
+z = 5
+
+resultx = round(x)  # 반올림
+print(resultx)
+
+resulty = abs(y)
+print(resulty)
+
+print(pow(4, 3))
+
+print(max(x,y,z))   # maximum value 찾음
+print(min(x,y,z))
+
+# import math 하면 math 모듈을 사용해 더 복잡한 함수나 상수들을 이용 가능
+print(math.pi)
+print(math.e)
+print(math.sqrt(9))
+print(math.ceil(3.13))  # 올림
+print(math.floor(3.93)) # 버림
+
+radius = float(input("Enter the radius of a circle: "))
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius, 2)
+
+print(f"The circumference is: {circumference}")
+print(f"The circumference is: {round(circumference, 2)}")
+
+print(f"The area is: {area}cm^2")
+print(f"The area is: {round(area, 3)}cm^2")
