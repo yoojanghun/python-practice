479035369f3f4114e4a750082efd476ecd2aa49b
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-04-05 23:35:50 +0900

    variables 복습

--- variables.py
+++ variables.py
@@ -1,17 +1,18 @@
-# Variable = A container for a value (data type: string, integer, float boolean)
+# Variable = A container for a value (data type: string, integer, float, boolean)
 #            A variable behaves as if it was the value it contains
+#            python에셔 변수 사이의 공간은 언더바로 처리하는 듯 하다.
 
 # Strings (문자열)
 first_name = "Yoo"
 print("first_name")
 print(first_name)
-print(f"Hello mr.{first_name}\n") # text와 변수를 함께 쓰는 방법
+print(f"Hello mr.{first_name}\n") # text와 변수를 함께 쓰는 방법 (f = format)
 
-#integer (= whole number)
+#integer (= whole number 정수)
 my_age = 24
 print(f"you are {my_age} year old\n")
 
-# float
+# float (= floating point number, 실수)
 price = 10.99
 print(f"The price is ${price}\n")
 
