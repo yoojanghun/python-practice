d71e41cb4bffbae3f08c126253897144b94f4fd3
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-04-06 00:22:35 +0900

    matlibs game

---
+++ matlibsGame.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective (description) : ")
+noun1 = input("Enter a noun (persion, place, thing) : ")
+adjective2 = input("Enter an adjective (description) : ")
+verb1 = input("Enter a verb ending with 'ing' : ")
+adjective3 = input("Enter an adjective (description) : ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!!")
--- practicing.py
+++
@@ -1,9 +0,0 @@
-# input() = 사용자로부터 입력 값을 받아 string으로 return 하는 함수
-#           입력 값을 이용하기 위해선 변수에 return된 값을 저장
-
-price = float(input("Enter the price : "))
-quantity = int(input("Enter the quantity : "))
-
-print(f"The total price is {price * quantity}")
-
-# float와 int를 곱한 결과는 float가 되는 듯 하다.
