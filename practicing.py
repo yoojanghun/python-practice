8c150a878692a77dacda717dd6ad95e72f5b4a47
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-04-06 18:57:03 +0900

    list, set, tuple 복습

--- practicing.py
+++ practicing.py
@@ -1,28 +1,43 @@
-# for loop = 우리가 원하는 숫자 만큼 코드를 실행
+# 집합형 자료구조 = 여러 값들을 저장하기 위한 하나의 변수
 
-for counter in range(1, 11, 3):
-    print(counter)
+# list  = 내가 아는 그 배열. 인덱스 있고, 요소 갱신(추가, 삭제, 수정) 가능, 중복 가능
+# set   = 내가 아는 그 집합. 인덱스 없고, 요소 수정 불가. 하지만 추가나 삭제는 가능, 중복 불가능
+# tuple = 인덱스는 있음. 하지만 요소 갱신(추가, 삭제, 수정) 불가능. 중복 가능
 
-for x in range(10, 0, -1):
-    print(x)
+# 그냥 list, set은 내가 아는 배열, 집합이고 나머지 tuple만 외우면 됨.
+# tuple은 갱신만 불가능. 나머진 다 있음. 인덱스도 있고, 중복도 가능함.
 
-credit_card = "1234-5678-9"
+# list
+# fruits = ["apple", "banana", "orange", "coconut", "coconut"]
 
-for x in credit_card:
-    print(x)
+# print(fruits[::-1])
 
-# continue와 break
-# continue : loop 중간에 건너뜀
-# break : loop를 빠져나옴
+# for fruit in fruits:
+#     print(fruit)
 
-for x in range(1, 21):
-    if x == 13:
-        continue
-    else:
-        print(x)
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits[0] = "pineapple"
+# print(len(fruits))
+# print(fruits.count("coconut"))
+# fruits.sort()
+# fruits.reverse()
+# fruits.insert(4, "pineapple")
+# fruits.clear()
 
-for x in range(1, 21):
-    if x == 13:
-        break
-    else:
-        print(x)
+# fruits = {"apple", "orange", "coconut", "banana"}
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# print("apple" in fruits)
+# print(len(fruits))
+
+# fruits = {"apple", "orange", "coconut", "banana", "banana"}
+
+fruits = ("apple", "apple", "orange", "coconut", "banana")
+
+# print(fruits.index("apple"))
+# print(fruits.index("coconut"))
+print(fruits.count("apple"))
+
+print(fruits)
