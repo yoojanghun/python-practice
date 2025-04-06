276d1a2742e2f609c87950ce3fab6d0ac1b98fb3
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-04-06 19:18:11 +0900

    function 복습

--- practicing.py
+++ practicing.py
@@ -1,43 +1,33 @@
-# 집합형 자료구조 = 여러 값들을 저장하기 위한 하나의 변수
+# function = 여러번 재사용 가능한 코드들
+#            함수를 호출 하기 위해선 () 사용
+#            함수를 호출할 때 인자(argument) 넣어줄 수 있음. 함수에서 매개변수(parameter)로 인자 값을 받음
+#            매개변수(parameter)의 순서는 중요하다
+#            return을 이용하여 함수에서 처리 된 값을 반환 가능하다. (return을 쓰면 함수가 종료된다.)
 
-# list  = 내가 아는 그 배열. 인덱스 있고, 요소 갱신(추가, 삭제, 수정) 가능, 중복 가능
-# set   = 내가 아는 그 집합. 인덱스 없고, 요소 수정 불가. 하지만 추가나 삭제는 가능, 중복 불가능
-# tuple = 인덱스는 있음. 하지만 요소 갱신(추가, 삭제, 수정) 불가능. 중복 가능
+# def happy_birthday(age, name):
+#     print(f"Happy birthday to {name}")
+#     print(f"Happy birthday to {name}")
+#     print(f"You are {age} old !!")
+#     print()
+#
+# happy_birthday("banana", 20)
+# happy_birthday("apple", 42)
+# happy_birthday("coconut", 62)
 
-# 그냥 list, set은 내가 아는 배열, 집합이고 나머지 tuple만 외우면 됨.
-# tuple은 갱신만 불가능. 나머진 다 있음. 인덱스도 있고, 중복도 가능함.
+# def add(x, y):
+#     return x + y
+#
+# def subtract(x, y):
+#     return x - y
+#
+# def multiply(x, y):
+#     return x * y
+#
+# def divide(x, y):
+#     return x / y
+#
+# print(add(2, 3))
+# print(subtract(2, 3))
+# print(multiply(2, 3))
+# print(divide(2, 3))
 
-# list
-# fruits = ["apple", "banana", "orange", "coconut", "coconut"]
-
-# print(fruits[::-1])
-
-# for fruit in fruits:
-#     print(fruit)
-
-# fruits.append("pineapple")
-# fruits.remove("apple")
-# fruits[0] = "pineapple"
-# print(len(fruits))
-# print(fruits.count("coconut"))
-# fruits.sort()
-# fruits.reverse()
-# fruits.insert(4, "pineapple")
-# fruits.clear()
-
-# fruits = {"apple", "orange", "coconut", "banana"}
-# fruits.add("pineapple")
-# fruits.remove("apple")
-# fruits.pop()
-# print("apple" in fruits)
-# print(len(fruits))
-
-# fruits = {"apple", "orange", "coconut", "banana", "banana"}
-
-fruits = ("apple", "apple", "orange", "coconut", "banana")
-
-# print(fruits.index("apple"))
-# print(fruits.index("coconut"))
-print(fruits.count("apple"))
-
-print(fruits)
