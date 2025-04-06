53adb86cde1b487d051f7e5d2af57dbe0985bc1d
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-04-06 13:01:56 +0900

    while loop 복습

--- practicing.py
+++ practicing.py
@@ -1,26 +1,33 @@
-# 논리 연산자: 여러 조건들을 계산할 수 있게 하는 연산자 (or, and, not)
+# while loop  = 어떤 조건이 참일 때, 코드를 계속 실행시킴.
 
-# 온도가 너무 덥거나 추우면, 또는 비가 내리면 야외활동 취소
-# temp = 25
-# is_raining = False
-#
-# if temp < 0 or temp > 25 or is_raining:
-#     print("The outdoor event is cancelled")
-# else:
-#     print("Have some fun")
+name = input("Enter your name : ")
 
-temp = 25
-is_sunny = False
+while name == "":
+    print("You didn't write your name")
+    name = input("Write your valid name : ")
 
-if temp >= 28 and is_sunny:
-    print("It is hot outside and it is sunny")
-elif temp <= 0 and is_sunny:
-    print("It is cold outside and it is sunny")
-elif 0 < temp < 28 and is_sunny:
-    print("It is warm outside and it is sunny")
-elif temp >= 28 and not is_sunny:
-    print("It is hot outside and it is not sunny")
-elif temp <= 0 and not is_sunny:
-    print("It is cold outside and it is not sunny")
-else:
-    print("It is warm outside and it is not sunny")
+print(f"Hello!! {name}!!")
+
+food = input("Enter the food you like (q to quit) : ")
+
+while not food == "q":
+    print(f"You like {food}!!!")
+    food = input("Enter another food (q to quit) : ")
+
+print("Bye")
+
+age = int(input("Enter your age : "))
+
+while age < 0:
+    print("Your age is not valid")
+    age = int(input("Enter your age again: "))
+
+print(f"You are {age} years old")
+
+number = int(input("Write your number from 1 to 10 : "))
+
+while number < 1 or number > 10:
+    print("Your number is not valid")
+    number = int(input("Write your number again : "))
+
+print(f"Your number is : {number}")
