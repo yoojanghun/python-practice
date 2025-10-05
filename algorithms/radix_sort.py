4c60b578f2f0d1e411a08b8897a7e6fb04e982be
yoojanghun <yoojanghun@naver.com>
2025-10-05 16:54:17 +0900

    radix sort

---
+++ algorithms/radix_sort.py
@@ -0,0 +1,5 @@
+# Radix Sort = 배열의 원소 중 아주 큰 값이 있으면 Counting Sort는 비효율적
+#              원소들이 모두 k 자릿수 이하인 음이 아닌 정수일 때,
+#              낮은 자릿수(LSD) 부터 높은 자릿수(MSD) 순서대로 자릿수를 기준으로 안정 정렬 수행 반복
+# 안정정렬(stable sort) = 같은 값들끼리 상대적인 순서를 유지하며 수행하는 정렬
+#                        ex) Bubble sort, Insertion sort, Merge sort, Counting sort 등
