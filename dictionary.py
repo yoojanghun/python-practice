e2fe0e7ef6bf8b53a10dfa0ea8736d920e6ed5c6
yoojanghun <yoojanghun@naver.com>
2025-10-06 00:31:07 +0900

    dictionary 변경

--- dictionary.py
+++ dictionary.py
@@ -11,23 +11,32 @@ capitals = {"USA": "Washington D.C.",
 # method와 attribute의 설명을 보려면
 # print(help(capitals))
 
-print(capitals.get("USA"))
-print(capitals.get("Japan"))
+print(capitals["USA"])
+print(capitals.get("Japan"))                # 잘 사용하지 않는 표현
+
+print()
 
 if capitals.get("Russia"):
     print("That capital exists")
 else:
     print("That capital doesn't exist")
 
+if "USA" in capitals:
+    print("That capital exists")
+else:
+    print("That capital doesn't exist")
+
+print()
+
 # dictionary의 값을 추가, 변경 가능
-capitals.update({"Germany": "Berlin"})
+capitals["Germany"] = "Berlin"
 print(capitals)
 
-capitals.update({"USA": "usa capital"})
+capitals["USA"] = "USA Capital"
 print(capitals)
 
 # key: value 쌍을 없애기 위해선
-capitals.pop("China")
+del capitals["China"]
 print(capitals)
 
 # 가장 최근의 쌍을 없애기 위해선
@@ -36,22 +45,34 @@ print(capitals)
 
 # capitals.clear()
 
+print()
+
 # dictionary의 모든 key를 얻으려면 (key 객체를 반환)
 keys = capitals.keys()
-print(keys)
+print(keys)                         # dict_keys(['USA', 'India', 'Russia'])
+keys = list(keys)
+print(keys)                         # ['USA', 'India', 'Russia']
 
 for key in capitals.keys():
     print(key)
 
+print()
+
 # dictionary의 모든 value를 얻으려면 (value 객체를 반환)
-values = capitals.values()
+values = capitals.values()         # dict_values(['usa capital', 'New Delhi', 'Moscow'])
+print(values)
+values = list(values)              # ['usa capital', 'New Delhi', 'Moscow']
 print(values)
 
 for value in capitals.values():
     print(value)
 
+print()
+
 # dictionary 객체를 반환 (Tuple의 2d list와 비슷한 모양)
-item = capitals.items()
+item = capitals.items()            # dict_items([('USA', 'usa capital'), ('India', 'New Delhi'), ('Russia', 'Moscow')])
+print(item)
+item = list(item)                  # [('USA', 'usa capital'), ('India', 'New Delhi'), ('Russia', 'Moscow')]
 print(item)
 
 for key, value in capitals.items():
